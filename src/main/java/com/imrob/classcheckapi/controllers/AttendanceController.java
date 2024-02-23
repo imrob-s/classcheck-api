4d13cbe32f5f7e84d03ed21b148d779c4525f60c
imrob-s <orobsilva@gmail.com>
2024-02-22 23:58:36 -0300

    add: Controllers classes

--- src/main/java/com/imrob/classcheckapi/controllers/AttendanceController.java
+++ src/main/java/com/imrob/classcheckapi/controllers/AttendanceController.java
@@ -1,6 +1,52 @@
 package com.imrob.classcheckapi.controllers;
 
+import com.imrob.classcheckapi.dto.AttendanceDTO;
+import com.imrob.classcheckapi.services.AttendanceService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
 public class AttendanceController {
 
+    @Autowired
+    private AttendanceService service;
+
+    @GetMapping
+    public ResponseEntity<AttendanceDTO> findById(@PathVariable Long id) {
+        AttendanceDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<AttendanceDTO>> findAll(Pageable pageable) {
+        Page<AttendanceDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<AttendanceDTO> insert(@Valid @RequestBody AttendanceDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.id()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<AttendanceDTO> update(@PathVariable Long id, @Valid @RequestBody AttendanceDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
--- src/main/java/com/imrob/classcheckapi/controllers/StudentController.java
+++ src/main/java/com/imrob/classcheckapi/controllers/StudentController.java
@@ -1,4 +1,50 @@
 package com.imrob.classcheckapi.controllers;
 
+import com.imrob.classcheckapi.dto.StudentDTO;
+import com.imrob.classcheckapi.services.StudentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
 public class StudentController {
+    @Autowired
+    private StudentService service;
+
+    @GetMapping
+    public ResponseEntity<StudentDTO> findById(@PathVariable Long id) {
+        StudentDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<StudentDTO>> findAll(Pageable pageable) {
+        Page<StudentDTO> dto = service.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<StudentDTO> insert(@Valid @RequestBody StudentDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.id()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<StudentDTO> update(@PathVariable Long id, @Valid @RequestBody StudentDTO dto){
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
